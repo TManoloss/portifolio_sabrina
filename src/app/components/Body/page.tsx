9aaa3ce1ab20302bd6461a3eb48824cab296eea6
Manoel Carvalho <manoelfelipe90@gmail.com>
2025-03-06 13:43:45 -0300

    fix: imagens de renders

--- src/app/components/Body/page.tsx
+++ src/app/components/Body/page.tsx
@@ -1,13 +1,16 @@
 import Apresentacao from "./Apresentacao/page"
 import About from "./About/page"
-
+import Portifolio from "./Portifolio/page"
 export default function body (){
     return(
         <div>
             <Apresentacao/>
 
-            <div className="flex justify-center items-center pt-20 w-full">
+            <div className="flex justify-center items-center pt-20 w-full pb-10">
                 <About/>
+            </div>
+            <div className="pb-10">
+                <Portifolio/>
             </div>
         </div>
     )
